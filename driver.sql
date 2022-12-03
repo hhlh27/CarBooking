7b54ee4a4805da41ffb5cdb652c8b322b827c80c
Hannah <104514493+hhlh27@users.noreply.github.com>
2022-12-03 14:32:37 +0800

    add driver sql db file

---
+++ driver.sql
@@ -0,0 +1,19 @@
+CREATE database driver_db;
+
+USE driver_db;
+
+CREATE TABLE Driver
+(
+DriverID 				varchar (5) NOT NULL,
+FirstName				varchar(50) 	NOT NULL,
+LastName				varchar(50) 	NOT NULL,
+IdenNo				varchar(9) 	NOT NULL,
+CarNo				varchar(8) 	NOT NULL,
+Mobile  	    	  int  	NOT NULL,
+Email		    	varchar(50)  	NOT NULL,
+AccPassword		    varchar(255)  	NOT NULL,
+CONSTRAINT PK_Driver PRIMARY KEY NONCLUSTERED (DriverID)
+);
+
+INSERT INTO Driver (DriverID, FirstName, LastName,IdenNo,CarNo, Mobile,Email, AccPassword) VALUES ("D0001", "Tom", "Lim",'S1234567A','SDF1234A', 92221111,'tomlee@gmail.com','password123');
+
