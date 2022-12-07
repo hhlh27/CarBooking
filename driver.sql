5e74eb0d8d9123d6cedc3b80713e68f084221e6e
Hannah <104514493+hhlh27@users.noreply.github.com>
2022-12-07 21:02:14 +0800

    update driver.sql

--- driver.sql
+++ driver.sql
@@ -4,10 +4,10 @@ USE driver_db;
 
 CREATE TABLE Driver
 (
-DriverID 				varchar (5) NOT NULL,
+DriverID 				varchar (9) NOT NULL,
 FirstName				varchar(50) 	NOT NULL,
 LastName				varchar(50) 	NOT NULL,
-IdenNo				varchar(9) 	NOT NULL,
+
 CarNo				varchar(8) 	NOT NULL,
 Mobile  	    	  varchar(8)  	NOT NULL,
 Email		    	varchar(50)  	NOT NULL,
@@ -15,5 +15,5 @@ AccPassword		    varchar(255)  	NOT NULL,
 CONSTRAINT PK_Driver PRIMARY KEY NONCLUSTERED (DriverID)
 );
 
-INSERT INTO Driver (DriverID, FirstName, LastName,IdenNo,CarNo, Mobile,Email, AccPassword) VALUES ("D0001", "Tom", "Lim",'S1234567A','SDF1234A', "92221111",'tomlee@gmail.com','password123');
+INSERT INTO Driver (DriverID, FirstName, LastName,IdenNo,CarNo, Mobile,Email, AccPassword) VALUES ('S1234567A', "Tom", "Lim",'SDF1234A', "92221111",'tomlee@gmail.com','password123');
 
