a8643c284242a2341b71c2ef9228b17fbcfd3f0a
Hannah <104514493+hhlh27@users.noreply.github.com>
2022-12-07 20:58:02 +0800

    edit console menu

--- driverConsole.go
+++ driverConsole.go
@@ -7,14 +7,12 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"strings"
 )
 
 type Driver struct { // map this type to the record in the table
 	DriverID  string
 	FirstName string
 	LastName  string
-	IdenNo    string
 	CarNo     string
 	Mobile    int
 	Email     string
@@ -25,96 +23,87 @@ type Drivers struct {
 }
 
 func main() {
-outer:
+	menu()
+}
+
+func listMenu() {
+	fmt.Println("1. Create New Driver Account")
+	fmt.Println("2. Update Driver Account")
+	fmt.Println("0. Exit")
+	fmt.Print("Enter an option: ")
+}
+func menu() {
+	var choose int
+	listMenu()
 	for {
-		fmt.Println(strings.Repeat("=", 10))
-		fmt.Println("Driver  Console\n",
-			"1. Create new driver account\n",
-			"2. Update driver account\n",
-			/* "3. Update passenger account\n",
-			"4. Delete course\n", */
-			"9. Quit")
-		fmt.Print("Enter an option: ")
+		fmt.Scan(&choose)
+		if choose == 0 {
+			fmt.Println("Thank You. Exiting...")
+			break
+		} else {
+			switch choose {
+			case 1:
+				create()
+				fmt.Println("")
+				bufio.NewReader(os.Stdin).ReadBytes('\n')
+				listMenu()
+			case 2:
+				update()
+				fmt.Println("")
+				bufio.NewReader(os.Stdin).ReadBytes('\n')
+				listMenu()
 
-		var choice int
-		fmt.Scanf("%d", &choice)
-
-		switch choice {
-		case 1:
-			create()
-		case 2:
-			update()
-		/* case 3:
-			update()
-		case 4:
-			delete() */
-		case 9:
-			break outer
-		default:
-			fmt.Println("### Invalid Input ###")
+			default:
+				fmt.Println("Re-enter your choice!")
+				listMenu()
+			}
 		}
 	}
 }
-
-/*
-	 func listAll() {
-		client := &http.Client{}
-
-		if req, err := http.NewRequest(http.MethodGet, "http://localhost:5000/api/v1/courses", nil); err == nil {
-			if res, err := client.Do(req); err == nil {
-				if body, err := ioutil.ReadAll(res.Body); err == nil {
-					var res Passengers
-					json.Unmarshal(body, &res)
-
-					for k, v := range res.Passengers {
-						fmt.Println(v.Name, "(", k, ")")
-						fmt.Println("Planned Intake:", v.Intake)
-						fmt.Println("Min GPA:", v.MinGPA)
-						fmt.Println("Max GPA:", v.MaxGPA)
-						fmt.Println()
-					}
-				}
-			} else {
-				fmt.Println(2, err)
-			}
-		} else {
-			fmt.Println(3, err)
-		}
-	}
-*/
 func create() {
 	var driver Driver
 	var driverID string
-	driverID = "D001"
 
+	//consoleReader := bufio.NewReader(os.Stdin)
+	fmt.Println()
+	fmt.Print("Enter your identification number: ")
+
+	fmt.Scan(&(driver.DriverID))
 	fmt.Print("Enter your first name: ")
-	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Scan(&(driver.FirstName))
+	/* reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
-	driver.FirstName = strings.TrimSpace(input)
-
+	driver.FirstName = strings.TrimSpace(input)   */
+	//fmt.Scanf("%s", &(driver.FirstName))
+	//fmt.Print("\n")
 	fmt.Print("Enter your last name: ")
-	fmt.Scanf("%s", &(driver.LastName))
-	fmt.Print("Enter your Identification number: ")
-	fmt.Scanf("%s", &(driver.IdenNo))
-	fmt.Print("Enter your car Number: ")
-	fmt.Scanf("%s", &(driver.CarNo))
-	fmt.Print("Enter your mobile number: ")
-	fmt.Scanf("%d", &(driver.Mobile))
-	fmt.Print("Enter your email: ")
-	fmt.Scanf("%s", &(driver.Email))
-	fmt.Print("Enter your password: ")
-	fmt.Scanf("%s", &(driver.Password))
 
+	fmt.Scan(&(driver.LastName))
+
+	fmt.Print("Enter your car Number: ")
+	fmt.Scan(&(driver.CarNo))
+	//fmt.Print("\n")
+	fmt.Print("Enter your mobile number: ")
+	fmt.Scan(&(driver.Mobile))
+	//fmt.Print("\n")
+	fmt.Print("Enter your email: ")
+	fmt.Scan(&(driver.Email))
+	fmt.Println()
+	//fmt.Print("\n")
+	fmt.Print("Enter your password: ")
+	fmt.Scan(&(driver.Password))
+	//fmt.Print("\n")
 	postBody, _ := json.Marshal(driver)
 	resBody := bytes.NewBuffer(postBody)
 
 	client := &http.Client{}
-	if req, err := http.NewRequest(http.MethodPost, "http://localhost:5000/api/v1/courses/"+driverID, resBody); err == nil {
+	if req, err := http.NewRequest(http.MethodPost, "http://localhost:5000/api/v1/drivers/"+driverID, resBody); err == nil {
 		if res, err := client.Do(req); err == nil {
 			if res.StatusCode == 202 {
-				fmt.Println("Course", driverID, "created successfully")
+				fmt.Println("driver", driverID, "created successfully")
 			} else if res.StatusCode == 409 {
-				fmt.Println("Error - course", driverID, "exists")
+				fmt.Println("Error - driver", driverID, "exists")
 			}
 		} else {
 			fmt.Println(2, err)
@@ -126,27 +115,44 @@ func create() {
 func update() {
 	var driver Driver
 	var driverID string
-	fmt.Print("Enter the ID of the account to be updated: ")
-	fmt.Scanf("%v", &driverID)
-	fmt.Print("Enter the new first name: ")
-	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println()
+	fmt.Print("Enter your identification number: ")
+
+	fmt.Scan(&(driver.DriverID))
+
+	fmt.Print("Enter your first name: ")
+
+	fmt.Scan(&(driver.FirstName))
+	/* reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
-	driver.FirstName = strings.TrimSpace(input)
+	driver.FirstName = strings.TrimSpace(input)   */
+	//fmt.Scanf("%s", &(driver.FirstName))
+	//fmt.Print("\n")
 	fmt.Print("Enter your last name: ")
-	fmt.Scanf("%s", &(driver.LastName))
-	fmt.Print("Enter your car number: ")
-	fmt.Scanf("%s", &(driver.CarNo))
+
+	fmt.Scan(&(driver.LastName))
+	//fmt.Print("\n")
+
+	//fmt.Print("\n")
+	fmt.Print("Enter your car Number: ")
+	fmt.Scan(&(driver.CarNo))
+	//fmt.Print("\n")
 	fmt.Print("Enter your mobile number: ")
-	fmt.Scanf("%d", &(driver.Mobile))
+	fmt.Scan(&(driver.Mobile))
+	//fmt.Print("\n")
 	fmt.Print("Enter your email: ")
-	fmt.Scanf("%s", &(driver.Email))
+	fmt.Scan(&(driver.Email))
+
+	//fmt.Println()
+	//fmt.Print("\n")
 	fmt.Print("Enter your password: ")
-	fmt.Scanf("%s", &(driver.Password))
+	fmt.Scan(&(driver.Password))
 
 	postBody, _ := json.Marshal(driver)
 
 	client := &http.Client{}
-	if req, err := http.NewRequest(http.MethodPut, "http://localhost:5000/api/v1/courses/"+driverID, bytes.NewBuffer(postBody)); err == nil {
+	if req, err := http.NewRequest(http.MethodPut, "http://localhost:5000/api/v1/drivers/"+driverID, bytes.NewBuffer(postBody)); err == nil {
 		if res, err := client.Do(req); err == nil {
 			if res.StatusCode == 202 {
 				fmt.Println("Driver", driverID, "updated successfully")
@@ -160,25 +166,3 @@ func update() {
 		fmt.Println(3, err)
 	}
 }
-
-/* func delete() {
-	var course string
-	fmt.Print("Enter the ID of the course to be deleted: ")
-	fmt.Scanf("%v", &course)
-
-	client := &http.Client{}
-	if req, err := http.NewRequest(http.MethodDelete, "http://localhost:5000/api/v1/courses/"+course, nil); err == nil {
-		if res, err := client.Do(req); err == nil {
-			if res.StatusCode == 200 {
-				fmt.Println("Course", course, "deleted successfully")
-			} else if res.StatusCode == 404 {
-				fmt.Println("Error - course", course, "does not exist")
-			}
-		} else {
-			fmt.Println(2, err)
-		}
-	} else {
-		fmt.Println(3, err)
-	}
-}
-*/
