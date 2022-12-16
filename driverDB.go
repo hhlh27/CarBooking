609c06a91ab12a63c6855b6b69e26221710d8a1f
Hannah <104514493+hhlh27@users.noreply.github.com>
2022-12-16 20:22:51 +0800

    edit driverDB

--- driverDB.go
+++ driverDB.go
@@ -15,23 +15,32 @@ import (
 )
 
 type Driver struct { // map this type to the record in the table
-	DriverID  string
+	//DriverID  string
 	FirstName string
 	LastName  string
-	IdenNo    string
 	CarNo     string
 	Mobile    string
 	Email     string
 	Password  string
 }
 
-func main() {
+var (
+	db  *sql.DB
+	err error
+)
 
+func main() {
+	db, err = sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/db")
+
+	if err != nil {
+		panic(err.Error())
+	}
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/driver/{driverid}", driver).Methods("GET", "POST", "PUT")
 	//router.HandleFunc("/api/v1/courses", allpassengers)
-	fmt.Println("Listening at port 5000")
-	log.Fatal(http.ListenAndServe(":5000", router))
+	router.HandleFunc("/api/v1/drivers", alldrivers)
+	fmt.Println("Listening at port 7000")
+	log.Fatal(http.ListenAndServe(":7000", router))
 }
 func driver(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
@@ -75,57 +84,138 @@ func driver(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 
+	} else if val, ok := isExist(params["driverid"]); ok {
+
+		json.NewEncoder(w).Encode(val)
+
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "Invalid driver ID")
 	}
 }
 func isExist(id string) (Driver, bool) {
-	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/driver_db")
+	/* //db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/driver_db")
 
 	// handle error
 	if err != nil {
 		panic(err.Error())
 	}
 
-	defer db.Close()
+	defer db.Close() */
+	var d Driver
 	results := db.QueryRow("select * from driver where id=?", id)
 
-	var d Driver
 	//var id string
-	err = results.Scan(&id, &d.FirstName, &d.LastName, &d.IdenNo, &d.CarNo, &d.Mobile, &d.Email, &d.Password)
+	err = results.Scan(&id, &d.FirstName, &d.LastName, &d.CarNo, &d.Mobile, &d.Email, &d.Password)
 	if err == sql.ErrNoRows {
 		return d, false
 	}
 	return d, true
 }
 func insertDriver(id string, d Driver) {
-	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/driver_db")
+	/* db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/driver_db")
 
 	// handle error
 	if err != nil {
 		panic(err.Error())
 	}
 
-	defer db.Close()
-	_, err = db.Exec("insert into driver values(?,?,?,?,?)", id, d.FirstName, d.LastName, d.IdenNo, d.CarNo, d.Mobile, d.Email, d.Password)
+	defer db.Close() */
+	_, err = db.Exec("insert into driver values(?,?,?,?,?)", id, d.FirstName, d.LastName, d.CarNo, d.Mobile, d.Email, d.Password)
 	if err != nil {
 		panic(err.Error())
 	}
 	//use curl to insert in cmd
 }
 func updateDriver(id string, d Driver) {
-	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/driver_db")
+	/* db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/driver_db")
 
 	// handle error
 	if err != nil {
 		panic(err.Error())
-	}
+	} */
 
-	defer db.Close()
+	//defer db.Close()
 	_, err = db.Exec("update driver set fname=?, lname=?,cNo=?, mobile=?,email=?,pw=? where id=?", d.FirstName, d.LastName, d.CarNo, d.Mobile, d.Email, d.Password, id)
 	if err != nil {
 		panic(err.Error())
 	}
 	//use curl to insert in cmd
+}
+func alldrivers(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+
+	/* found := false
+	results := map[string]Driver{} */
+
+	if value := query.Get("q"); len(value) > 0 {
+		//for k, v := range drivers {
+		/* 	if strings.Contains(strings.ToLower(v.FirstName), strings.ToLower(value)) {
+				results[k] = v
+				found = true
+			}
+		} */
+		results, found := queryDrivers(value)
+
+		if !found {
+			fmt.Fprintf(w, "No driver found")
+		} else {
+			json.NewEncoder(w).Encode(struct {
+				Results map[string]Driver `json:"driver Search Results"`
+			}{results})
+		}
+
+	} else {
+		driversWrapper := struct {
+			Drivers map[string]Driver `json:"Drivers"`
+		}{getDrivers()}
+		json.NewEncoder(w).Encode(driversWrapper)
+		return
+	}
+}
+func getDrivers() map[string]Driver {
+	results, err := db.Query("select * from Driver")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var drivers map[string]Driver = map[string]Driver{}
+
+	for results.Next() {
+		var d Driver
+		var id string
+		err = results.Scan(&id, &d.FirstName, &d.LastName, &d.CarNo, &d.Mobile, &d.Email)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		drivers[id] = d
+	}
+
+	return drivers
+}
+
+func queryDrivers(query string) (map[string]Driver, bool) {
+	results, err := db.Query("select * from Driver where lower(firstname) like lower(?)", "%"+query+"%")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var drivers map[string]Driver = map[string]Driver{}
+
+	for results.Next() {
+		var d Driver
+		var id string
+		err = results.Scan(&id, &d.FirstName, &d.LastName, &d.CarNo, &d.Mobile, &d.Email)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		drivers[id] = d
+	}
+
+	if len(drivers) == 0 {
+		return drivers, false
+	}
+	return drivers, true
 }
