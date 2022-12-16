a0746ce89543f17985db60433c7a928ef0c6ef3d
Hannah <104514493+hhlh27@users.noreply.github.com>
2022-12-16 20:34:33 +0800

    update booking.sql

--- booking.sql
+++ booking.sql
@@ -1,4 +1,4 @@
-drop database booking_db;
+
 CREATE database booking_db;
 
 USE booking_db;
@@ -6,12 +6,12 @@ USE booking_db;
 
 CREATE TABLE Booking
 (
-  BookingID				varchar (9) NOT NULL,
+  BookingID				varchar (20) NOT NULL,
   PassengerID			varchar (9)     NOT NULL,
-DriverID			varchar (5)      NOT NULL,
+DriverID			varchar (9)      NOT NULL,
 PickUp			varchar (6)     NOT NULL,
 DropOff		varchar (6)     NOT NULL,
-BookingDateTime		datetime		NOT NULL ,
+BookingDateTime		varchar(20) 		NOT NULL ,
 BookingStatus     	varchar(15)  	NOT NULL,
   CONSTRAINT PK_Booking PRIMARY KEY NONCLUSTERED (BookingID)
 
