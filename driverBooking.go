74ba8f141bcb4e7a96ce55f5bc8de1ada1a55249
Hannah <104514493+hhlh27@users.noreply.github.com>
2022-12-16 20:29:59 +0800

    update driver booking console

--- driverBooking.go
+++ driverBooking.go
@@ -5,19 +5,18 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
-	"time"
 )
 
 type Booking struct { // map this type to the record in the table
-	ID              string
-	PassengerID     string
-	DriverID        string
-	PickUp          string
-	DropOff         string
-	BookingDateTime time.Time
-	BookingStatus   string
+	PassengerID     string `json:"Passenger Id"`
+	DriverID        string `json:"Driver Id"`
+	PickUp          string `json:"Pickup"`
+	DropOff         string `json:"Dropoff"`
+	BookingDateTime string `json:"Booking DateTime"`
+	BookingStatus   string `json:"Booking Status"`
 }
 
 type Bookings struct {
@@ -29,8 +28,9 @@ func main() {
 
 }
 func listMenu() {
-	fmt.Println("1. Initiate start strip")
-	fmt.Println("2. End Trip")
+	fmt.Println("1. View pending bookings")
+	fmt.Println("2. Initiate start trip")
+	fmt.Println("3. End Trip")
 
 	fmt.Println("0. Exit")
 	fmt.Print("Enter an option: ")
@@ -46,11 +46,16 @@ func menu() {
 		} else {
 			switch choose {
 			case 1:
-				InitiateStartTrip()
+				ViewPendingBookings()
 				fmt.Println("")
 				bufio.NewReader(os.Stdin).ReadBytes('\n')
 				listMenu()
 			case 2:
+				InitiateStartTrip()
+				fmt.Println("")
+				bufio.NewReader(os.Stdin).ReadBytes('\n')
+				listMenu()
+			case 3:
 				EndTrip()
 				fmt.Println("")
 				bufio.NewReader(os.Stdin).ReadBytes('\n')
@@ -63,11 +68,42 @@ func menu() {
 		}
 	}
 }
+func ViewPendingBookings() {
+
+	client := &http.Client{}
+
+	if req, err := http.NewRequest(http.MethodGet, "http://localhost:5003/api/v1/pendingbookings", nil); err == nil {
+		if res, err := client.Do(req); err == nil {
+			if body, err := ioutil.ReadAll(res.Body); err == nil {
+				var res Bookings
+				json.Unmarshal(body, &res)
+
+				for k, v := range res.Bookings {
+					fmt.Println(v.BookingDateTime, "(", k, ")")
+					fmt.Println("PickUp postal code", v.PickUp)
+					fmt.Println("DropOff postal code", v.DropOff)
+					fmt.Println("Booking Date Time", v.BookingDateTime)
+					fmt.Println("Passenger ID", v.PassengerID)
+
+					fmt.Println()
+				}
+			}
+		} else {
+			fmt.Println(2, err)
+		}
+	} else {
+		fmt.Println(3, err)
+	}
+}
 func InitiateStartTrip() {
 	var booking Booking
 	var bookingID string
 	//driverID = "D0001"
-
+	fmt.Println()
+	fmt.Print("Enter Booking ID to initiate trip: ")
+	fmt.Scan(&(bookingID))
+	fmt.Print("Enter Driver ID: ")
+	fmt.Scan(&(booking.DriverID))
 	//booking.DriverID = driverID
 	booking.BookingStatus = "Confimed"
 
@@ -75,12 +111,12 @@ func InitiateStartTrip() {
 	resBody := bytes.NewBuffer(postBody)
 
 	client := &http.Client{}
-	if req, err := http.NewRequest(http.MethodPut, "http://localhost:5000/api/v1/bookings/"+bookingID, resBody); err == nil {
+	if req, err := http.NewRequest(http.MethodPut, "http://localhost:5003/api/v1/bookings/"+bookingID, resBody); err == nil {
 		if res, err := client.Do(req); err == nil {
 			if res.StatusCode == 202 {
 				fmt.Println("Booking", bookingID, "confirmed successfully")
 			} else if res.StatusCode == 404 {
-				fmt.Println("Error - Booking", booking.ID, "does not exist")
+				fmt.Println("Error - Booking", bookingID, "does not exist")
 			}
 		} else {
 			fmt.Println(2, err)
@@ -94,8 +130,9 @@ func EndTrip() {
 	//var driverID string
 	var bookingID string
 	//driverID = "D0001"
-	bookingID = "B0001"
-
+	//bookingID = "B0001"
+	fmt.Print("Enter Booking ID to end trip: ")
+	fmt.Scan(&(bookingID))
 	//booking.DriverID = driverID
 	booking.BookingStatus = "Completed"
 
@@ -103,7 +140,7 @@ func EndTrip() {
 	resBody := bytes.NewBuffer(postBody)
 
 	client := &http.Client{}
-	if req, err := http.NewRequest(http.MethodPut, "http://localhost:5000/api/v1/bookings/"+bookingID, resBody); err == nil {
+	if req, err := http.NewRequest(http.MethodPut, "http://localhost:5003/api/v1/bookings/"+bookingID, resBody); err == nil {
 		if res, err := client.Do(req); err == nil {
 			if res.StatusCode == 202 {
 				fmt.Println("Booking", bookingID, "completed")
